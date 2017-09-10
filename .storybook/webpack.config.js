8aba3b407d9de9361280bb4a63893e5dc44ff125
Heleth <bon.hoo@hotmail.com>
2017-09-10 11:18:08 +0800

    chore: add custom webpack config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = (storybookBaseConfig, configType) => {
+  storybookBaseConfig.module.rules.push({
+    test: /\.(scss|css)$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, '../')
+  })
+
+  return storybookBaseConfig
+}
